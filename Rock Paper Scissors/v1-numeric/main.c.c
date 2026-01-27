40436cfe3ec722f4fdea5795825ace429e194552
serine <madjoudj.serine@gmail.com>
2026-01-27 21:04:53 +0100

    Reorganizing my project structure

---
+++ Rock Paper Scissors/v1-numeric/main.c.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void getComputerChoice(int *input){
+
+    *input = (rand() % 3) + 1; 
+
+}
+
+void getHumanChoice(int *input){
+      
+    do
+    {
+        printf("Please enter your choice.\n");
+        printf("Choose: 1 = Rock, 2 = Paper, 3 = Scissors\n");
+        scanf("%d", input);  
+    } while (*input < 1 || *input > 3);
+    
+
+}
+
+void PlayRound(int human, int computer){
+
+   if (human == computer){
+    printf("Draw!!\n");
+   }else if (
+    (human == 2 && computer == 1) ||
+    (human == 1 && computer == 3) ||
+    (human == 3 && computer == 2)
+   ){
+    printf("YOU WIN !!! \n");
+   }else{
+    printf("Computer WINS !!! \n");
+   }
+
+}
+
+
+int main(){
+
+    srand(time(NULL));
+
+    int humanChoice;
+    int computerChoice;
+
+    getHumanChoice(&humanChoice);
+    getComputerChoice(&computerChoice);
+
+    printf("Human chose: %d\n", humanChoice);
+    printf("Computer chose: %d\n", computerChoice);
+
+
+    PlayRound(humanChoice, computerChoice);
+
+    return 0;
+
+}
