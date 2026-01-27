c75643b3b8429d010a8dabd4d6f8582898f2c622
abdelrzz9 <ladjiciabdelrazzak@gmail.com>
2026-01-27 18:12:24 +0100

    fix: problam in function playRaond

--- abdouMiniDevs/aymenCha.c
+++ abdouMiniDevs/aymenCha.c
@@ -4,6 +4,7 @@
 #include<time.h>
 #include<ctype.h> 
 #include<stdbool.h>
+    // it work
     void getComCho(char *chcpCho){
         const char *name;
         int random = rand() % 3 +1;
@@ -20,18 +21,18 @@
         }
         strcpy(chcpCho,name);
     }
+    // problam
     void getHumanCho(char *input){
         char name[9];
         while(1){
             printf("Enter ur chois \n");
             fgets(name,sizeof(name),stdin);
-            int i =sizeof(name);
-            printf("%d",i);
+            name[strcspn(name,"\n")] = '\0';
             for(int i = 1 ; name[i];i++){
                 name[i] = tolower(name[i]);
             }
             name[0] = toupper(name[0]);
-            if(strcmp(name,"Rock") == 1|| strcmp(name,"Paper") == 1 ||strcmp(name,"Scissors") == 1){
+            if(strcmp(name,"Rock") == 0|| strcmp(name,"Paper") == 0 ||strcmp(name,"Scissors") == 0){
                 strcpy(input,name);
                 break;
             }else{
@@ -39,52 +40,61 @@
             }
         }    
 }
-    void playRaond(){
+    // it work 
+    void playRaond(bool *input1 , bool *input2){
         char humanChoice[9];
         char cmpChoice[9];
         bool humanWin = false;
-        bool tel = false;
+        bool Tie = false;
         getHumanCho(humanChoice);
         getComCho(cmpChoice);
         if (strcmp(humanChoice,cmpChoice) == 0){
-            tel = true;
-            printf("");
+            Tie = true;
+            printf("tie !!!\n");
         }else{
-            switch (humanChoice[1])
+            switch (humanChoice[0])
                 {
                     case 'R':
                             if(cmpChoice[0] == 'P'){
-                                printf("u lost paper wins");
+                                printf("u lost paper wins\n");
                         }else{
                             humanWin = true;
-                            printf("u win sissors lost");
+                            printf("u win sissors lost\n");
                         }
                     break;
-        
                     case 'S':
                             if(cmpChoice[0] == 'R'){
-                                printf("u lost rock wins");
+                                printf("u lost rock wins\n");
                             }else{
                                 humanWin = true;
-                                printf("u win paper lost");
+                                printf("u win paper lost\n");
                         }
                     break;
                     case 'P':
                             if(cmpChoice[0] == 'S'){
-                                printf("u lost sissors wins");
+                                printf("u lost sissors wins\n");
                             }else{
                                 humanWin = true;
-                                printf("u win");
+                                printf("u win\n");
                             }
                     break;
                     default:
-                    printf("error");
+                    printf("error\n");
                     break;
                 }
         }
+        *input1 = Tie;
+        *input2 = humanWin;
 }
-    
+
 
     int main(){
+        bool human_isWin;
+        bool draw;
 
+
+        playRaond(&draw,&human_isWin);
 }
+
+
+
