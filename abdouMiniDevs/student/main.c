0284fbc76884065af77d5fca61efcbe8b738b88e
abdelrzz9 <ladjiciabdelrazzak@gmail.com>
2026-01-31 02:33:21 +0100

    add: display student function and fix somm error

--- abdouMiniDevs/student/main.c
+++ abdouMiniDevs/student/main.c
@@ -5,77 +5,98 @@
 #include<ctype.h> 
 #include<stdbool.h>
 struct student{
-    double id ;
+    char id[16] ;
     char name[50];
     int age ;
     char major[50];
 
 };
-// last student func it work
-void find_last_student(int *index_of_last_student ,struct student students[1000] ){
+// it works
+void accept_stuedent(char *name , char *major , int *age){
+    int iLaSt;
+    char name_local[50];
+    int age_local ;
+    char major_local[50];
+    int i = 0;
+    printf("Enter u student name\n");
+    fgets(name_local,sizeof(name_local),stdin);
+    name_local[strcspn(name_local,"\n")] = '\0';
+    printf("Enter u student major\n");
+    fgets(major_local,sizeof(major_local),stdin);
+    major_local[strcspn(major_local,"\n")] = '\0';
+        do
+    {
+        printf("Enter u student age\n");
+        scanf("%d",&age_local);
+        getchar();
+    } while (age_local <= 16 ||age_local >= 80);
+    *age = age_local;
+    strcpy(name,name_local);
+    strcpy(major,major_local);
+}
+// it works
+void  id_student(char *id){
+    char chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$^&*qwertyuioplkjhgfdasxcvbnm";
+    for(int i = 0;i<15;i++ ){
+        id[i] = chars[rand() % (sizeof(chars)-1)];
+    }
+    id[15] = '\0';
+    
+} 
+// it works
+void find_last_student(int *index_void ,struct student students[1000] ){
     int i =0;
     while(i < 1000){
         if(students[i].age == 0){
-            *index_of_last_student = i;
+            *index_void = i;
             break;
         }
         i++;
     }
 }
-
-
-void add_student(int *age , char *name ,char *major){
-    char name_local[50];
-    char mjor_local[50];
-    int age_local;
-    printf("Enter u student name\n");
-    fgets(name_local,sizeof(name_local),stdin);
-    name_local[strcspn(name_local,"\n")] = '\0';
-    printf("Enter u student major\n");
-    fgets(mjor_local,sizeof(mjor_local),stdin);
-    mjor_local[strcspn(mjor_local,"\n")] = '\0';
-    strcpy(name,name_local);
-    strcpy(major,mjor_local);
-    printf("Enter u student age\n");
-    scanf("%d",&age_local);
-    getchar();
-    *age = age_local;
+// it work
+void add_student(struct student students[1000]){
+    int iLaSt;
+    find_last_student(&iLaSt,students);
+    accept_stuedent(students[iLaSt].name,students[iLaSt].major,&students[iLaSt].age);
 }
+void sw(struct student students[1000]){
 
+
+}
 // find student func
-void delete_student(){
+void delete_student(struct student students[1000]){
+    int iLast ,age, i =0;
+    char name[50];
+    char major[50];
+    accept_stuedent(name,major,&age);
+    for ( ; i < iLast ;i++){
+        if( name == students[i].name && age == students[i].age && major == students[i].major ){
+            break;
+        }
+    }
 
 }
 
-
-
-
-
-
-
-
-
+// it work
+void  displaying_students(struct student students[1000]){
+    int iLaSt;
+    find_last_student(&iLaSt,students);
+        printf("====================\n");
+    for(int i = 0 ; i < iLaSt ; i++){
+        printf("student number : %d\n",i + 1);
+        printf("%s\n",students[i].name);
+        printf("%s\n",students[i].major);
+        printf("%d\n",students[i].age);
+        printf("%s\n",students[i].id);
+        printf("====================\n");
+    }
+}
 
 
 
 int main(){
-    struct student students[1000]; 
-    int last_index = 0; 
-    for(int i = 0 ;i<19;i++){
-        students[i]=(struct student){
-            .age = 24,
-            .name ="abderzz9",
-            .major = "ing"
-        };
-    }
-    for(int i = 0 ; i < 19 ;i++){
-        printf("%s\n",students[i].name);
-        printf("%s\n",students[i].major);
-        printf("%d\n",students[i].age);
-    }
-    printf("=====================\n");
-    printf("%d\n",students[19].age);
-    printf("=====================\n");
-    find_last_student(&last_index, students);
-    printf("%d",last_index);
+    srand(time(NULL));
+
+
 }
