86cb9440ed31238d40192380df836e13da4219ee
abdelrzz9 <ladjiciabdelrazzak@gmail.com>
2026-02-15 22:13:02 +0100

    sovle exo 2

--- abdouMiniDevs/tds/exo2.c
+++ abdouMiniDevs/tds/exo2.c
@@ -53,15 +53,48 @@ void read_R(struct Resident *a){
     read_D(&a->Date_Birth);
     read_A(&a->Residence);
 }
-
 int main(){
     struct Resident residents[100];
     int n ;
+    char w[50];
+    char v[50];
     do
     {
+        printf("Enter the number n : ");
         scanf("%d",&n);
     } while (n <= 0 || n > 100);
-    for(int i =0 ; i < 2 ;i++){
+    for(int i =0 ; i < n ;i++){
         read_R(&residents[i]);
     }
+    int AN ;
+    printf("Enter the an : ");
+    scanf("%d",&AN);
+    printf("entr city\n");
+    ReadInput(w,50); 
+    printf("entr pr\n");
+    ReadInput(v,50);
+    printf("data de barthi\n");
+    for(int i = 0 ; i < n ; i++){
+        if(AN > residents[i].Date_Birth.Year){
+            printf("%s\n",residents[i].Residence.City);
+            printf("%s\n",residents[i].Residence.Street);
+            printf("%s\n",residents[i].Residence.Provinc);
+            printf("%d\n",residents[i].Residence.Pc);
+            printf("%d\n",residents[i].Residence.Number);
+        }
+    }
+    printf("===============\n");
+    printf("cyit and pr");
+    for(int i = 0 ; i < n ; i++){
+        if(strcmp(residents[i].Residence.City , w )&& strcmp(residents[i].Residence.Provinc,v)){
+            printf("%s %s %d/%d/%d",residents[i].First_Name
+                ,residents[i].Last_Name,
+                residents[i].Date_Birth.Day,
+                residents[i].Date_Birth.Month,
+                residents[i].Date_Birth.Year
+            );
+            printf("================\n");
+        }
+    }
+    printf("-----------------------------\n");
 }
